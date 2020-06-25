6cf5bb77331f0fcf3b55bc2855f4a7263a889620
Tommaso <tommaso.vsr@gmail.com>
2020-06-25 18:24:18 +0200

    version fix

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends Component {
 	}
 
 	onSubmitSignUp = () => {
-		fetch('http://localhost:3001/register', {
+		fetch('https://morning-castle-assessment-api.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
